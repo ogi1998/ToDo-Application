08eedfdf6a645c780530a287bcee2c6cd53a2641
ogi1998 <ogivuja@gmail.com>
2022-09-06 12:40:54 +0200

    refactored read method to always split line of data instead of splitting in every "list" function

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -3,58 +3,51 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-
 interface DataManipulator {
-    void manipulateData(String line);
+    void manipulateData(String[] fields);
 }
 public class CSVReader {
-
     String filePath;
-
     CSVReader(String filePath) {
         this.filePath = filePath;
     }
-
     void read(DataManipulator dm) {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
 
             while ((line = br.readLine()) != null) {
-                dm.manipulateData(line);
+                String[] fields = line.split(",");
+                dm.manipulateData(fields);
             }
         } catch (IOException ex) {
             System.err.println("ERROR: File can't be read!");
         }
     }
-
     void listTasks() {
         StringBuilder result = new StringBuilder("");
 
         System.out.println("List of all tasks: ");
         System.out.println("\tid\t|\tstatus\t|\tcreated/modified timestamp\t|\t name\t|\tpriority\t|");
 
-        read(line -> {
-            String[] attrs = line.split(",");
-            for (String attr: attrs) {
-                result.append('\t').append(attr.trim()).append('\t').append('|');
+        read(fields -> {
+            for (String field: fields) {
+                result.append('\t').append(field.trim()).append('\t').append('|');
             }
             result.append('\n');
         });
 
         System.out.println(result);
     }
-
     void listTasksByStatus() {
         boolean shouldPrintSeperator = false;
         ArrayDeque<TaskByStatus> tasksByStatus = new ArrayDeque<>();
 
         System.out.println("Tasks listed by status (not done ([]) > done ([X]):");
 
-        read(line -> {
-            String[] splittedLine = line.split(",");
-            if (splittedLine[1].trim().equals("done"))
-                tasksByStatus.add(new TaskByStatus(splittedLine[3].trim(), "[X}"));
-            else tasksByStatus.addFirst(new TaskByStatus(splittedLine[3].trim(), "[]"));
+        read(fields -> {
+            if (fields[1].trim().equals("done"))
+                tasksByStatus.add(new TaskByStatus(fields[3].trim(), "[X}"));
+            else tasksByStatus.addFirst(new TaskByStatus(fields[3].trim(), "[]"));
         });
 
         while (!tasksByStatus.isEmpty()) {
@@ -70,9 +63,8 @@ public class CSVReader {
     void listTasksByPriority() {
         PriorityQueue<TaskByPriority> tasksByPriority = new PriorityQueue<>(new TaskByPriorityComparator());
         System.out.println("Tasks listed by priority (high > normal > low):");
-        read(line -> {
-            String[] splittedLine = line.split(",");
-            tasksByPriority.add(new TaskByPriority(splittedLine[3].trim(), splittedLine[4].trim()));
+        read(fields -> {
+            tasksByPriority.add(new TaskByPriority(fields[3].trim(), fields[4].trim()));
         });
 
         while (!tasksByPriority.isEmpty()) {
@@ -80,19 +72,17 @@ public class CSVReader {
             System.out.println(task.getName() + ", " + task.getPriority());
         }
     }
-
     void listTasksByTimestamp() {
         TreeMap<Long, String> tasksByTimestamp = new TreeMap<>(Comparator.reverseOrder());
 
         System.out.println("Tasks listed by timestamp (newest first):");
 
-        read(line -> {
-            String[] splittedLine = line.split(",");
+        read(fields -> {
 
-            LocalDateTime date = LocalDateTime.parse(splittedLine[2].trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+            LocalDateTime date = LocalDateTime.parse(fields[2].trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
             long milliseconds = date.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
 
-            tasksByTimestamp.put(milliseconds, splittedLine[3]);
+            tasksByTimestamp.put(milliseconds, fields[3]);
         });
 
         for (Map.Entry<Long, String> entry: tasksByTimestamp.entrySet()) {
